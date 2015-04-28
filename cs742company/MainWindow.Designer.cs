4fdf9942ee4a08c264348a32867f0416aa635cca
Han Chen <alvinchen8611@gmail.com>
2015-04-28 16:52:52 -0500

    Working on GUI

--- cs742company/MainWindow.Designer.cs
+++ cs742company/MainWindow.Designer.cs
@@ -28,21 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.ReadFile = new System.Windows.Forms.Button();
+            this.ExceptionReportor = new System.Windows.Forms.TextBox();
+            this.ExceptionLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // ReadFile
+            // 
+            this.ReadFile.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ReadFile.Location = new System.Drawing.Point(12, 12);
+            this.ReadFile.Name = "ReadFile";
+            this.ReadFile.Size = new System.Drawing.Size(155, 33);
+            this.ReadFile.TabIndex = 0;
+            this.ReadFile.Text = "ReadFile";
+            this.ReadFile.UseVisualStyleBackColor = true;
+            this.ReadFile.Click += new System.EventHandler(this.ReadFile_Click);
+            // 
+            // ExceptionReportor
+            // 
+            this.ExceptionReportor.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ExceptionReportor.Location = new System.Drawing.Point(127, 359);
+            this.ExceptionReportor.Multiline = true;
+            this.ExceptionReportor.Name = "ExceptionReportor";
+            this.ExceptionReportor.Size = new System.Drawing.Size(735, 68);
+            this.ExceptionReportor.TabIndex = 1;
+            // 
+            // ExceptionLabel
+            // 
+            this.ExceptionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ExceptionLabel.Location = new System.Drawing.Point(12, 359);
+            this.ExceptionLabel.Name = "ExceptionLabel";
+            this.ExceptionLabel.Size = new System.Drawing.Size(100, 23);
+            this.ExceptionLabel.TabIndex = 2;
+            this.ExceptionLabel.Text = "Exception";
+            this.ExceptionLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.ExceptionLabel);
+            this.Controls.Add(this.ExceptionReportor);
+            this.Controls.Add(this.ReadFile);
             this.Name = "MainWindow";
             this.Text = "MainWindow";
             this.Load += new System.EventHandler(this.MainWindow_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button ReadFile;
+        private System.Windows.Forms.TextBox ExceptionReportor;
+        private System.Windows.Forms.Label ExceptionLabel;
 
     }
 }
--- cs742company/MainWindow.cs
+++ cs742company/MainWindow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,36 @@ namespace cs742company
         private void MainWindow_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void ReadFile_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            ExceptionReportor.Text = "Loading file successful.";
+                            // Insert code to read the stream here.
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ExceptionReportor.Text ="Error: Could not read file from disk. Original error: " + ex.Message;
+                }
+            }
         }
     }
 }
